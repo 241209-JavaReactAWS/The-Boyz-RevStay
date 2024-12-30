ecdb4b206e04b3571b8076111d591928f6997c72
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-12-30 14:33:11 -0500

    implementing notifications

---
+++ backend/src/main/java/com/Revature/RevStay/daos/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Revature.RevStay.daos;
+
+import com.Revature.RevStay.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
--- backend/src/main/java/com/Revature/RevStay/models/User.java
+++ backend/src/main/java/com/Revature/RevStay/models/User.java
@@ -1,9 +1,24 @@
 package com.Revature.RevStay.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name= "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private UserRole role;
 }
